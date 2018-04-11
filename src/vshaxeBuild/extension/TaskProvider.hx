2a3fadf3042a65880ccfa06cb9c9834651c97a65
Jens Fischer <jensfischer95@gmail.com>
2018-04-11 22:33:42 +0200

    Add an "install dependencies" task

--- src/vshaxeBuild/extension/TaskProvider.hx
+++ src/vshaxeBuild/extension/TaskProvider.hx
@@ -16,32 +16,43 @@ class TaskProvider {
             if (target.name == "empty" || target.name == "vshaxe-node") {
                 continue; // hack: skip stuff from defaults.json
             }
-            tasks.push(createTask(target, false));
+            tasks.push(createTaskForTarget(target, false));
             if (!target.args.debug) {
-                tasks.push(createTask(target, true));
+                tasks.push(createTaskForTarget(target, true));
             }
         }
+
+        if (projects.length > 1 && projects[1].mainTarget != null) {
+            tasks.push(createTask("install dependencies", projects[1].mainTarget, false, ["--mode", "install"]));
+        }
+
         return tasks;
     }
 
-    function createTask(target:Target, debug:Bool):Task {
+    function createTaskForTarget(target:Target, debug:Bool):Task {
         var suffix = "";
         if (!target.args.debug && debug) suffix = " (debug)";
+        return createTask(target.name + suffix, target.name, debug);
+    }
 
+    function createTask(name:String, target:String, debug:Bool, ?additionalArgs:Array<String>):Task {
         var definition:VSHaxeBuildTaskDefinition = {
             type: "vshaxe-build",
-            target: target.name
+            target: name
         };
         if (debug) {
             definition.debug = true;
         }
 
-        var args = ["run", "vshaxe-build", "--target", target.name];
+        var args = ["run", "vshaxe-build", "--target", target];
+        if (additionalArgs != null) {
+            args = args.concat(additionalArgs);
+        }
         if (vshaxe.displayPort != null && vshaxe.enableCompilationServer) {
             args = args.concat(["--connect", Std.string(vshaxe.displayPort)]);
         }
         var execution = new ShellExecution("haxelib", args, {env: vshaxe.haxeExecutable.configuration.env});
-        var task = new Task(definition, target.name + suffix, "vshaxe-build", execution, vshaxe.problemMatchers.get());
+        var task = new Task(definition, name, "vshaxe-build", execution, vshaxe.problemMatchers.get());
         var presentation = vshaxe.taskPresentation;
         task.presentationOptions = {
             reveal: presentation.reveal,
