5836bbfa187b7d0bc0d2ffbe8587071533931d14
Jens Fischer <jensfischer95@gmail.com>
2019-06-13 21:32:02 +0200

    Update to json2object 3.6.3 for schema generation

--- src/vshaxeBuild/tools/SchemaGenerator.hx
+++ src/vshaxeBuild/tools/SchemaGenerator.hx
@@ -1,14 +1,10 @@
 package vshaxeBuild.tools;
 
 import sys.io.File;
-import json2object.utils.JsonSchemaWriter;
-
-using StringTools;
+import json2object.utils.special.VSCodeSchemaWriter;
 
 class SchemaGenerator {
 	static function main() {
-		var schema = new JsonSchemaWriter<Project>("\t").schema;
-		schema = schema.replace('"description"', '"markdownDescription"');
-		File.saveContent("schemas/vshaxe-build-schema.json", schema);
+		File.saveContent("schemas/vshaxe-build.schema.json", new VSCodeSchemaWriter<Project>("\t").schema);
 	}
 }
